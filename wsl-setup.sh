838fe048780c6622df5255536e936483337e6ea3
KacperMisztal <69186452+KacperMisztal@users.noreply.github.com>
2024-04-05 10:55:06 +0200

    deleted old script

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-# Script links user's hhome folder directories to home folder directories in WSL
-# WARNING: This script assumes that your user name in Windows and WSL is the same
-
-for dir in 'Documents' 'Pictures' 'Downloads' 'Videos' 'Desktop' 'Music' '3D Objects'
-do
-	ln -s "/mnt/c/Users/$USER/$dir" "/home/$USER/$dir"
-	echo /mnt/c/Users/$USER/$dir /home/$USER/$dir
-done
